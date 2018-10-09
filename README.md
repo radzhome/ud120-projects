6c95ab116c2102808ce45dddcdf1d48e27cce165
radzhome <radzhome@gmail.com>
2018-10-09 11:49:29 -0400

    mini project lesson 1 complete

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+### To create env
+pipenv --python 2.7.15
+
--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,18 +16,35 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+# train
+# create train set (fit) based on what we know is for sure,
+# feature train set and the target labels
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+# predict
+# create prediction based on feature test data
+t0 = time()
+pr = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+# accuracy
+# label test has the correct labels for each point
+# check how well they math up with the predicted data
+from sklearn.metrics import accuracy_score
+score = accuracy_score(labels_test, pr)
+print(score)
 #########################################################
 
 
